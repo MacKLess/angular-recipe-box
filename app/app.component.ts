11e3c651fa8cf7f8a4e38e2e9b684c74c7d38b8e
Kristen Kulha and LDMacKrell <kristen@>
2017-10-24 08:39:12 -0700

    Update app to include app.component.

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: './app.component.html',
+})
+
+export class AppComponent {
+  recipes: Recipe[] = [
+    new Recipe('Scrambled Eggs', 'eggs, mushrooms, cheese', 'put eggs and mushroom in a pan, cook, add cheese'),
+    new Recipe('Toast', 'bread, butter, jam', 'toast bread, butter, and add jam'),
+    new Recipe('Tea', 'teabag and hot water', 'boil water and add tea bag')
+  ];
+}
+
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string) { }
+}
